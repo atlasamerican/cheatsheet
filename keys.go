13c86483fbd28686eb5bb3ee681135512f29df50
James Reed <james@twiddlingbits.net>
2022-05-29 12:58:47 -0600

    Implement key hints

--- keys.go
+++ keys.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gdamore/tcell/v2"
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/gdamore/tcell/v2"
+)
 
 type KeyPress struct {
 	key tcell.Key
@@ -8,6 +14,53 @@ type KeyPress struct {
 }
 
 type KeyMap map[KeyPress]string
+
+func generateHint(cmd string, kps ...KeyPress) string {
+	keys := make([]string, 0, len(kps))
+	for _, kp := range kps {
+		if kp.key == tcell.KeyRune {
+			keys = append(keys, string(kp.ch))
+		} else {
+			keys = append(keys, tcell.KeyNames[kp.key])
+		}
+	}
+	sort.Strings(keys)
+
+	return fmt.Sprintf("[black:white]%s[-:-] %s", strings.Join(keys, ","), cmd)
+}
+
+func (km KeyMap) generateHintKey() string {
+	for kp, cmd := range km {
+		if cmd == "hint" {
+			return generateHint(cmd, kp)
+		}
+	}
+	return ""
+}
+
+func (km KeyMap) generateHints() []string {
+	order := make([]string, 0)
+	cmds := make(map[string][]KeyPress)
+
+	for kp, cmd := range km {
+		if kps, ok := cmds[cmd]; ok {
+			cmds[cmd] = append(kps, kp)
+		} else {
+			kps = make([]KeyPress, 1)
+			kps[0] = kp
+			cmds[cmd] = kps
+			order = append(order, cmd)
+		}
+	}
+
+	sort.Strings(order)
+
+	hs := make([]string, 0, len(cmds))
+	for _, cmd := range order {
+		hs = append(hs, generateHint(cmd, cmds[cmd]...))
+	}
+	return hs
+}
 
 func (km KeyMap) event2command(ev *tcell.EventKey) (string, bool) {
 	key := ev.Key()
