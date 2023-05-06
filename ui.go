c3df72b5c3a2b1f5812c54a8d4af60fa2b4dc505
James Reed <james@twiddlingbits.net>
2023-05-06 17:14:29 -0400

    Show update feedback
    Reworks UI to better handle messages.

--- ui.go
+++ ui.go
@@ -33,7 +33,7 @@ type UI struct {
 	maxPerColumn  int
 	viewer        *Viewer
 	dataset       *Dataset
-	errors        int
+	logLength     int
 	keyMap        KeyMap
 	hintKey       string
 	hints         string
@@ -83,7 +83,8 @@ type Footer struct {
 	*tview.Flex
 	ui     *UI
 	page   *tview.TextView
-	info   *tview.TextView
+	hints  *tview.TextView
+	log    *tview.TextView
 	height int
 }
 
@@ -93,14 +94,20 @@ func newFooter(ui *UI) *Footer {
 		ui,
 		// page
 		tview.NewTextView().SetTextAlign(tview.AlignRight),
-		// info
+		// hints
+		tview.NewTextView().
+			SetDynamicColors(true).SetWrap(true).SetWordWrap(true).
+			SetTextAlign(tview.AlignLeft),
+		// log
 		tview.NewTextView().
 			SetDynamicColors(true).SetWrap(true).SetWordWrap(true).
 			SetTextAlign(tview.AlignLeft),
 		3,
 	}
+
 	f.AddItem(f.page, 1, 1, false)
-	f.AddItem(f.info, 0, 1, false)
+	f.AddItem(f.hints, 0, 1, false)
+	f.AddItem(f.log, 0, 1, false)
 
 	// Force redraw when footer page changes.
 	f.page.SetChangedFunc(func() {
@@ -132,15 +139,19 @@ func (f *Footer) updatePage(i int, more bool) {
 	f.page.SetText(s)
 }
 
-func (ui *UI) resetFooter(full bool) {
-	ui.footer.info.Clear()
+func (ui *UI) clearLog() {
+	ui.footer.log.Clear()
+	ui.logLength = 0
+}
+
+func (ui *UI) resetHints(full bool) {
+	ui.footer.hints.Clear()
 	if full {
-		ui.footer.info.SetText(ui.fullHints)
+		ui.footer.hints.SetText(ui.fullHints)
 	} else {
-		ui.footer.info.SetText(ui.hints)
+		ui.footer.hints.SetText(ui.hints)
 	}
 	ui.fullHinting = full
-	ui.errors = 0
 }
 
 func (ui *UI) viewTldr(c Command) {
@@ -161,20 +172,18 @@ func (ui *UI) unviewTldr() {
 }
 
 func (ui *UI) toggleHints() {
-	ui.resetFooter(!ui.fullHinting)
+	ui.resetHints(!ui.fullHinting)
 }
 
-func (ui *UI) showError(msg string) {
-	ui.fullHinting = false
-
+func (ui *UI) showMessage(msg string, color string) {
 	var text string
-	if ui.errors > 0 {
-		text = ui.footer.info.GetText(true) + "\n"
+	if ui.logLength > 0 {
+		text = ui.footer.log.GetText(true) + "\n"
 	}
-	ui.footer.info.Clear()
+	ui.footer.log.Clear()
 
-	ui.footer.info.SetText("[red]" + text + tview.Escape(msg))
-	ui.errors++
+	ui.footer.log.SetText(color + text + tview.Escape(msg))
+	ui.logLength++
 }
 
 func (ui *UI) handleKey(ev *tcell.EventKey) *tcell.EventKey {
@@ -190,7 +199,8 @@ func (ui *UI) handleKey(ev *tcell.EventKey) *tcell.EventKey {
 		ui.toggleHints()
 		return ev
 	case "clear":
-		ui.resetFooter(false)
+		ui.resetHints(false)
+		ui.clearLog()
 		return ev
 	case "quit":
 		ui.app.Stop()
@@ -246,16 +256,28 @@ func (ui *UI) routerChanged() {
 }
 
 func newUI(config Config) *UI {
+
+	dataset, updated := newDataset(config)
+
 	ui := &UI{
 		app:           tview.NewApplication(),
 		root:          tview.NewFlex().SetDirection(tview.FlexRow),
 		router:        tview.NewPages(),
-		dataset:       newDataset(config),
+		dataset:       dataset,
 		commandPagers: make(map[string]*Pager[Command]),
 		keyMap:        config.keyMap,
 		maxPerColumn:  -1,
 	}
 	ui.footer = newFooter(ui)
+
+	go func() {
+		status, ok := <-updated
+		if ok && status {
+			ui.app.QueueUpdateDraw(func() {
+				ui.showMessage("Updated! Restart required", "[green]")
+			})
+		}
+	}()
 
 	ui.router.SetChangedFunc(ui.routerChanged)
 
@@ -274,13 +296,14 @@ func newUI(config Config) *UI {
 	ui.router.AddPage(PAGE_SECTIONS, ui.sectionPager, true, true)
 	ui.router.AddPage(PAGE_VIEWER, ui.viewer, true, false)
 
-	ui.resetFooter(false)
+	ui.resetHints(false)
+	ui.clearLog()
 	ui.app.SetRoot(ui.root, true)
 
 	go func() {
 		for msg := range logger.queue {
 			ui.app.QueueUpdateDraw(func() {
-				ui.showError(msg)
+				ui.showMessage(msg, "[red]")
 			})
 		}
 	}()
